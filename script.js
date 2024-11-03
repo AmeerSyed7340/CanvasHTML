81681fb2bf21c98080bb45002b6c19580a6dea01
AmeerSyed7340 <Ameer.Syed40@qmail.cuny.edu>
2024-11-02 20:40:02 -0400

    Added smiley face using canvas

--- script.js
+++ script.js
@@ -27,3 +27,24 @@ ctx.lineTo(0,150);
 ctx.lineTo(100,100);
 
 ctx.stroke();
+
+
+var c2 = document.getElementById("text-canvas");
+var context = c2.getContext("2d");
+
+context.beginPath();
+//body
+context.arc(150,75, 70, 0, 2*Math.PI);
+context.fillStyle = 'pink';
+context.fill();
+
+context.moveTo(140,55);
+context.arc(130,55, 10, 0, 2*Math.PI);
+
+context.moveTo(180,55);
+context.arc(170, 55, 10, 0, 2 * Math.PI);
+
+context.moveTo(200, 75);
+context.arc(150, 75, 50, 0,  Math.PI);
+
+context.stroke();
